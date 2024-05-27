2e250b174b5491ceca91a7cafe74f2271034d1a3
davitkhela <datuna0987654321@gmail.com>
2024-05-27 22:56:20 +0400

    second commit

---
+++ day1.py
@@ -0,0 +1,61 @@
+name = 'Davit'
+last_name = 'kheladze'
+age = '13'
+hobby = 'riding a bike and playing card games'
+ 
+ #now print the variables
+
+print(name)
+print(last_name)
+print(age)
+print(hobby)
+
+
+first = 'Hello '
+second = 'I study in GOA'
+print(first+second)
+
+#Let's do some math now
+
+x1 = 5
+y1 = 5
+print(x1+y1)
+
+x2 = 7
+y2 = 7
+print(x2+y2)
+
+m = 50
+n = 35
+print(m-n)
+
+m2 = 150
+n2 = 70
+print(m2-n2)
+
+a = 7
+b = 20
+print(a*b)
+
+a2 = 50
+b2 = 7
+print(a2*b2)
+
+g = 1000
+h = 50
+print (g//h)
+
+g2 = 4000
+h2 = 80
+print(g2//h2)
+
+c = 2
+d = 10
+print(c**d)
+
+c2 = 7
+d2 = 5
+print(c2**d2)
+
+
+
