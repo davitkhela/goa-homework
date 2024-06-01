e5721f90effa7b9967b245c4e328bdbc3c443921
davitkhela <datuna0987654321@gmail.com>
2024-06-01 22:47:35 +0400

    second commit

---
+++ day 1/day1 hw.py
@@ -0,0 +1,92 @@
+name = 'davit'
+last_name = 'kheladze'
+age = 13
+hobby = 'riding a bike'
+country = 'Georgia'
+
+print(name)
+print(last_name)
+print(age)
+print(hobby)
+print(country)
+
+first = 'hello '
+second = 'i study in GOA'
+print(first+second)
+
+#დამატება
+
+print(10+10)
+print(20+30)
+print(40+10)
+print(100+150)
+print(70+50)
+print(10+90)
+print(1000+700)
+print(1010+1100)
+print(7770+80)
+print(109+101)
+
+#გამოკლება
+
+print(10-5)
+print(100-50)
+print(20-17)
+print(500-300)
+print(100-55)
+print(43-21)
+print(313-312)
+print(5050-5000)
+print(1000-575)
+print(100-99)
+print(7-5)
+
+#გამრავლება
+
+print(2*5)
+print(20*50)
+print(200*500)
+print(5*7)
+print(40*80)
+print(20*500)
+print(4*33)
+print(8*5000)
+print(2*500000000)
+print(4*700)
+print(1*10000000000)
+
+#მთლიანი გაყოფა
+
+print(4//2)
+print(40//20)
+print(400//200)
+print(100//20)
+print(1000//200)
+print(10000//2000)
+print(1010//5)
+print(1011010//5)
+print(1100010//5)
+print(101010//5)
+print(101111110//5)
+
+#ხარისხები
+
+print(5**5)
+print(5**7)
+print(5**8)
+print(5**0)
+print(7**2)
+print(7**3)
+print(7**4)
+print(7**5)
+print(7**7)
+print(10101*2)
+print(12345**2)
+
+
+
+
+
+
+
+
